8fb544cb685abbc25fffb2e86816387ebc4b890d
biaoli <biaoli6@hotmail.com>
2023-07-08 22:08:19 -0400

    1 file modified

--- app/data/image.service.ts
+++ app/data/image.service.ts
@@ -1,8 +1,25 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable()
+// export class ImageService {
+
+//   constructor() { }
+
+// }
+
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ImageService {
+  private apiUrl = 'https://picsum.photos';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getRandomImage(width: number, height: number): Observable<string> {
+    const url = `${this.apiUrl}/${width}/${height}`;
+
+    return this.http.get(url, { responseType: 'text' });
+  }
 }
