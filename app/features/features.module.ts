0a68e898684af09e466dbfbebf2214c87f877700
biaoli <biaoli6@hotmail.com>
2023-07-09 17:47:53 -0400

    1 file modified

--- app/features/features.module.ts
+++ app/features/features.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [{ path: '', component: ProductComponent }];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [ProductComponent],
+  providers: [HttpClientModule],
 })
 export class FeaturesModule {}
