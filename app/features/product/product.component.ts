ab1b6573624ab391a4a3b00be58a51ce17e014f0
biaoli <biaoli6@hotmail.com>
2023-07-08 22:53:59 -0400

    2 files modified

--- app/features/product/product.component.ts
+++ app/features/product/product.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../../data/image.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
+  // constructor() { }
+  randomImageUrl: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private imageService: ImageService) {
+    this.imageService
+      .getRandomImage(500, 300)
+      .subscribe((url) => (this.randomImageUrl = url));
   }
 
+  ngOnInit() {}
 }
