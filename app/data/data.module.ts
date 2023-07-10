9fc404ffcc753589c36a3f7b4e08732f70a73bdc
biaoli <biaoli6@hotmail.com>
2023-07-10 10:02:06 -0400

    1 file modified

--- app/data/data.module.ts
+++ app/data/data.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ImageService } from './image.service';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
+  //declarations: [],
+  providers: [ImageService],
 })
 export class DataModule {}
