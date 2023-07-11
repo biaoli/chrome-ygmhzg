0d746a37e987e8006deca75e9591def0fd5b757e
biaoli <biaoli6@hotmail.com>
2023-07-10 23:25:16 -0400

    2 files modified

--- app/features/features.module.ts
+++ app/features/features.module.ts
@@ -4,11 +4,16 @@ import { ProductComponent } from './product/product.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ImageService } from '../data/image.service';
+import { CarouselModule } from 'ngx-bootstrap';
 
 const routes: Routes = [{ path: '', component: ProductComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    CarouselModule.forRoot(),
+  ],
   declarations: [ProductComponent],
   providers: [ImageService],
 })
